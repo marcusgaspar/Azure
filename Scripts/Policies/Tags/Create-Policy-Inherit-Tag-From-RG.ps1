4b52a2055ae8f43be4714499ac1702e8b83b3353
Marcus Gaspar <marcusgaspar@hotmail.com>
2023-06-15 00:31:16 -0300

    Updates on Apply Tags and New Inherit Tag from RG

--- Scripts/Policies/Tags/Create-Policy-Inherit-Tag-From-RG.ps1
+++ Scripts/Policies/Tags/Create-Policy-Inherit-Tag-From-RG.ps1
@@ -3,14 +3,15 @@ $SubscriptionID="50e6fbdc-6729-4bbc-9cdb-7fc8b7b32311"
 $TenantID="16b3c013-d300-468d-ac64-7eda0820b6d3"
 $Location="eastus"
 # xlsx file with TagName and TagValue columns 
-$TagFile = "C:\_repo\Azure\Scripts\Policies\Tags\Tags-RGs.xlsx"
+$TagFile = "C:\_repo\Azure\Scripts\Policies\Tags\Tags-Subscriptions.xlsx"
 # Log file
 $LogFile = "C:\_repo\Azure\Scripts\Policies\Tags\Create-Policies.log"
 ################################
 
-$PolicyNamePrefix="TagRG"
-$PolicyDescription="Adds or replaces the specified tag and value when any resource group is created or updated. Existing resource groups can be remediated by triggering a remediation task."
-$PolicyDefinitionId="d157c373-a6c4-483d-aaad-570756956268"
+$PolicyNamePrefix="InheritTagRG"
+$PolicyDefinitionDescription="Adds or replaces the specified tag and value from the parent resource group when any resource is created or updated. Existing resources can be remediated by triggering a remediation task."
+$PolicyScope="/subscriptions/$SubscriptionID"
+$PolicyDefinitionId="cd3aa116-8754-49c9-a813-ad46512ece54"
 
 $RemediationNamePrefix="remediation-task"
 
@@ -33,30 +34,24 @@ $TagData = Import-Excel -Path $TagFile
 # Loop through each row and create a policy
 foreach ($row in $TagData) {
     $TagName = $row.TagName
-    $TagValue = $row.TagValue
+    
     $SubscriptionName=$row.SubscriptionName
-    $RGName=$row.ResourceGroupName
-    $SubscriptionNameShort=$SubscriptionName.Substring(0,4)
-    $PolicyName="$PolicyNamePrefix-$SubscriptionNameShort-$RGName-$TagName"
+    $PolicyName="$PolicyNamePrefix-$SubscriptionName-$TagName"
     $datetimestring=Get-Date -Format "yyyyMMddHHmmss"
     $RemediationName="$RemediationNamePrefix-$datetimestring"
 
-    # Get the subscription ID
+    #Get the subscription ID
     $SubscriptionID = (Get-AzSubscription -SubscriptionName $SubscriptionName).Id
     
-    # Get the RG resource ID
-    $RGReourceId = (Get-AzResourceGroup -Name $RGName).ResourceId
-
     # Create the policy using the tag name and value
-    $PolicyParamTag = @{tagName = @{value = $TagName}; tagValue = @{value = $TagValue}}
+    $PolicyParamTag = @{tagName = @{value = $TagName}}
     $PolicyParamTagJson = $PolicyParamTag | ConvertTo-Json
-
+    
     Write-Host "Creating new PolicyAssignment..." -ForegroundColor Green
-    # Create the policy assignment    
-    $PolicyScope=$RGReourceId
+    # Create the policy assignment
     $Policy = Get-AzPolicyDefinition -Name $PolicyDefinitionId
-        
-    $PolicyAssignment = New-AzPolicyAssignment -Name $PolicyName -DisplayName $PolicyName -Description $PolicyDescription -Scope $PolicyScope -PolicyDefinition $Policy -PolicyParameter $PolicyParamTagJson -Location $Location -AssignIdentity
+    
+    $PolicyAssignment = New-AzPolicyAssignment -Name $PolicyName -DisplayName $PolicyName -Description $PolicyDefinitionDescription -Scope $PolicyScope -PolicyDefinition $Policy -PolicyParameter $PolicyParamTagJson -Location $Location -AssignIdentity
     $PolicyAssignmentId = $PolicyAssignment.PolicyAssignmentId
 
     $Principal = $null
@@ -65,7 +60,7 @@ foreach ($row in $TagData) {
         $PrincipalID=$PolicyAssignment.Identity.principalId
         Write-Host "Checking if Service Principal was already created..." -ForegroundColor Green
         $Principal = Get-AzADServicePrincipal -ObjectId $PrincipalID
-                
+
         # Check if the application was found
         if ($null -ne $Principal) {
             $PrincipalID=$Principal.Id
@@ -75,23 +70,25 @@ foreach ($row in $TagData) {
             try {
                 # Create Role Assignment to Grant Contributor role to the Policy system assigned identity on the scope of subscription
                 # It is required by Remediation Task to apply the Tags on the resources
-                New-AzRoleAssignment -ErrorAction Stop -ObjectId $PrincipalID -RoleDefinitionName Contributor -Scope $PolicyScope 
+                New-AzRoleAssignment -ErrorAction Stop -ObjectId $PrincipalID -RoleDefinitionName Contributor -Scope $PolicyScope                 
                 break
             }
             catch {
                 Write-Host "Error creating role assignment: $_" -ForegroundColor Red
                 Write-Host "Retrying in 10 seconds..."
                 Start-Sleep -Seconds 10
-            }            
+            }
+
+            
         } else {
             Write-Host "The service principal $PrincipalID does not exist. Waiting for 10 seconds..."
             Start-Sleep -Seconds 10
         }
     }
-   
+    
     Write-Host "Creating new Policy Remediation Task..." -ForegroundColor Green
     # Create the remediation task
-    Start-AzPolicyRemediation -Name $RemediationName -PolicyAssignmentId $PolicyAssignmentId -ResourceDiscoveryMode ReEvaluateCompliance -Scope $PolicyScope
+    Start-AzPolicyRemediation -Name $RemediationName -PolicyAssignmentId  $PolicyAssignmentId -ResourceDiscoveryMode ReEvaluateCompliance -Scope $PolicyScope
 
 }   
 
